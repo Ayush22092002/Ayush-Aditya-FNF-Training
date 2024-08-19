8df563784b0a39714bee7f3e9205c17420175ad7
AYUSH ADITYA <80828762+Ayush22092002@users.noreply.github.com>
2024-08-19 17:40:36 +0530

    Add files via upload

---
+++ Models/GameTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PraticeSwagger.Models
+{
+	[Table("GameTable")]
+	public class GameTable
+    {
+        [Key]
+        public int GameId { get; set; }
+
+        public string GameName { get; set; } = null!;
+
+        public double GamePrice { get; set; }
+    }
+
+	public class GameDbContext : DbContext
+	{
+		public DbSet<GameTable> Games { get; set; }
+
+		public GameDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+	}
+
+}
